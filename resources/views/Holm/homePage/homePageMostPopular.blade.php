d3105da051e3e5d17c9e8a2e3d792545cc49515c
Yarchick <yyarchick@gmail.com>
2017-09-01 11:20:31 +0300

    fixed bugs

--- resources/views/Holm/homePage/homePageMostPopular.blade.php
+++ resources/views/Holm/homePage/homePageMostPopular.blade.php
@@ -8,150 +8,293 @@
                 Check some of our popular carers below
             </p>
         </div>
-        <div class="sliderContainer">
-            <a href="" class="sliderControl sliderControl--left centeredLink">
-                <i class="fa fa-angle-left"></i>
-            </a>
-            <a href="" class="sliderControl sliderControl--right centeredLink">
-                <i class="fa fa-angle-right"></i>
-            </a>
-            <div class="popularSlider">
-                <div class="popularSlider__item popularCard">
-                    <div class="profilePhoto  ">
-                        <img src="./img/profile.png" alt="">
-                    </div>
-                    <div class="profileRating popularCard__rating">
-              <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                        <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                        <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                        <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                        <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                    </div>
-                    <h2 class="profileName">
-                        JOHN A.
-                    </h2>
-                    <p class="popularCard__info">
-                        [one line summary]
-                    </p>
-                    <p class="hourPrice">
-              <span class="hourPrice__price">
-                £ 12</span><span class="hourPrice__timing">/hour</span>
-                    </p>
+
+
+
+
+      <div class="carousel slide multi-item-carousel" id="theCarousel">
+        <div class="carousel-inner">
+          <div class="item active">
+
+              <div class="popularSlider__item popularCard">
+                <div class="profilePhoto  ">
+                    <img src="./img/profile.png" alt="">
                 </div>
-
-                <div class="popularSlider__item popularCard">
-                    <div class="profilePhoto  ">
-                        <img src="./img/profile2.png" alt="">
-                    </div>
-                    <div class="profileRating popularCard__rating">
-              <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                        <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                        <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                        <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                        <span class="profileRating__item">
-                <i class="fa fa-heart"></i>
-              </span>
-                    </div>
-                    <h2 class="profileName">
-                        KEITH S.
-                    </h2>
-                    <p class="popularCard__info">
-                        [one line summary]
-                    </p>
-                    <p class="hourPrice">
-              <span class="hourPrice__price">
-                £ 12</span><span class="hourPrice__timing">/hour</span>
-                    </p>
+                <div class="profileRating popularCard__rating">
+                  <span class="profileRating__item active">
+                    <i class="fa fa-heart"></i>
+                  </span>
+                            <span class="profileRating__item active">
+                    <i class="fa fa-heart"></i>
+                  </span>
+                            <span class="profileRating__item active">
+                    <i class="fa fa-heart"></i>
+                  </span>
+                            <span class="profileRating__item active">
+                    <i class="fa fa-heart"></i>
+                  </span>
+                            <span class="profileRating__item active">
+                    <i class="fa fa-heart"></i>
+                  </span>
                 </div>
+                <h2 class="profileName">
+                    JOHN A.
+                </h2>
+                <p class="popularCard__info">
+                    [one line summary]
+                </p>
+                <p class="hourPrice">
+                  <span class="hourPrice__price">
+                    £ 11</span><span class="hourPrice__timing">/hour</span>
+                </p>
+              </div>
 
-                <div class="popularSlider__item popularCard">
-                    <div class="profilePhoto  ">
-                        <img src="./img/profile3.png" alt="">
-                    </div>
-                    <div class="profileRating popularCard__rating">
-              <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                        <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                        <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                        <span class="profileRating__item ">
-                <i class="fa fa-heart"></i>
-              </span>
-                        <span class="profileRating__item">
-                <i class="fa fa-heart"></i>
-              </span>
-                    </div>
-                    <h2 class="profileName">
-                        MEL C.
-                    </h2>
-                    <p class="popularCard__info">
-                        [one line summary]
-                    </p>
-                    <p class="hourPrice">
-              <span class="hourPrice__price">
-                £ 12</span><span class="hourPrice__timing">/hour</span>
-                    </p>
+              <div class="popularSlider__item popularCard">
+                  <div class="profilePhoto  ">
+                    <img src="./img/profile2.png" alt="">
+                  </div>
+                  <div class="profileRating popularCard__rating">
+                  <span class="profileRating__item active">
+                    <i class="fa fa-heart"></i>
+                  </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item">
+              <i class="fa fa-heart"></i>
+            </span>
+                  </div>
+                  <h2 class="profileName">
+                      KEITH S.
+                  </h2>
+                  <p class="popularCard__info">
+                      [one line summary]
+                  </p>
+                  <p class="hourPrice">
+            <span class="hourPrice__price">
+              £ 11</span><span class="hourPrice__timing">/hour</span>
+                  </p>
+              </div>
+
+              <div class="popularSlider__item popularCard">
+                  <div class="profilePhoto  ">
+                      <img src="./img/profile3.png" alt="">
+                  </div>
+                  <div class="profileRating popularCard__rating">
+            <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item ">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item">
+              <i class="fa fa-heart"></i>
+            </span>
+                  </div>
+                  <h2 class="profileName">
+                      MEL C.
+                  </h2>
+                  <p class="popularCard__info">
+                      [one line summary]
+                  </p>
+                  <p class="hourPrice">
+            <span class="hourPrice__price">
+              £ 12</span><span class="hourPrice__timing">/hour</span>
+                  </p>
+              </div>
+
+              <div class="popularSlider__item popularCard">
+                  <div class="profilePhoto  ">
+                      <img src="./img/profile4.png" alt="">
+                  </div>
+                  <div class="profileRating popularCard__rating">
+            <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                  </div>
+                  <h2 class="profileName">
+                      ROSIE P.
+                  </h2>
+                  <p class="popularCard__info">
+                      [one line summary]
+                  </p>
+                  <p class="hourPrice">
+            <span class="hourPrice__price">
+              £ 11</span><span class="hourPrice__timing">/hour</span>
+                  </p>
+              </div>
+
+          </div>
+          <div class="item">
+
+              <div class="popularSlider__item popularCard">
+                <div class="profilePhoto  ">
+                    <img src="./img/profile.png" alt="">
                 </div>
-
-                <div class="popularSlider__item popularCard">
-                    <div class="profilePhoto  ">
-                        <img src="./img/profile4.png" alt="">
-                    </div>
-                    <div class="profileRating popularCard__rating">
-              <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                        <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                        <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                        <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                        <span class="profileRating__item active">
-                <i class="fa fa-heart"></i>
-              </span>
-                    </div>
-                    <h2 class="profileName">
-                        ROSIE P.
-                    </h2>
-                    <p class="popularCard__info">
-                        [one line summary]
-                    </p>
-                    <p class="hourPrice">
-              <span class="hourPrice__price">
-                £ 12</span><span class="hourPrice__timing">/hour</span>
-                    </p>
+                <div class="profileRating popularCard__rating">
+                  <span class="profileRating__item active">
+                    <i class="fa fa-heart"></i>
+                  </span>
+                            <span class="profileRating__item active">
+                    <i class="fa fa-heart"></i>
+                  </span>
+                            <span class="profileRating__item active">
+                    <i class="fa fa-heart"></i>
+                  </span>
+                            <span class="profileRating__item active">
+                    <i class="fa fa-heart"></i>
+                  </span>
+                            <span class="profileRating__item active">
+                    <i class="fa fa-heart"></i>
+                  </span>
                 </div>
+                <h2 class="profileName">
+                    JOHN A.
+                </h2>
+                <p class="popularCard__info">
+                    [one line summary]
+                </p>
+                <p class="hourPrice">
+                  <span class="hourPrice__price">
+                    £ 11</span><span class="hourPrice__timing">/hour</span>
+                </p>
+              </div>
+
+              <div class="popularSlider__item popularCard">
+                  <div class="profilePhoto  ">
+                    <img src="./img/profile2.png" alt="">
+                  </div>
+                  <div class="profileRating popularCard__rating">
+                  <span class="profileRating__item active">
+                    <i class="fa fa-heart"></i>
+                  </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item">
+              <i class="fa fa-heart"></i>
+            </span>
+                  </div>
+                  <h2 class="profileName">
+                      KEITH S.
+                  </h2>
+                  <p class="popularCard__info">
+                      [one line summary]
+                  </p>
+                  <p class="hourPrice">
+            <span class="hourPrice__price">
+              £ 11</span><span class="hourPrice__timing">/hour</span>
+                  </p>
+              </div>
+
+              <div class="popularSlider__item popularCard">
+                  <div class="profilePhoto  ">
+                      <img src="./img/profile3.png" alt="">
+                  </div>
+                  <div class="profileRating popularCard__rating">
+            <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item ">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item">
+              <i class="fa fa-heart"></i>
+            </span>
+                  </div>
+                  <h2 class="profileName">
+                      MEL C.
+                  </h2>
+                  <p class="popularCard__info">
+                      [one line summary]
+                  </p>
+                  <p class="hourPrice">
+            <span class="hourPrice__price">
+              £ 12</span><span class="hourPrice__timing">/hour</span>
+                  </p>
+              </div>
+
+              <div class="popularSlider__item popularCard">
+                  <div class="profilePhoto  ">
+                      <img src="./img/profile4.png" alt="">
+                  </div>
+                  <div class="profileRating popularCard__rating">
+            <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                      <span class="profileRating__item active">
+              <i class="fa fa-heart"></i>
+            </span>
+                  </div>
+                  <h2 class="profileName">
+                      ROSIE P.
+                  </h2>
+                  <p class="popularCard__info">
+                      [one line summary]
+                  </p>
+                  <p class="hourPrice">
+            <span class="hourPrice__price">
+              £ 11</span><span class="hourPrice__timing">/hour</span>
+                  </p>
+              </div>
+
+          </div>
 
 
+      </div>
 
-            </div>
-        </div>
+      <a href="#theCarousel" data-slide="prev" class="sliderControl sliderControl--left centeredLink">
+        <i class="fa fa-angle-left"></i>
+      </a>
+
+      <a href="#theCarousel" data-slide="next" class="sliderControl sliderControl--right centeredLink">
+        <i class="fa fa-angle-right"></i>
+      </a>
 
     </div>
 </section>
