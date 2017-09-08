5c8f2ea40740121713fc1c25ab6e1a587d3a9ec5
PetrErshov <user@ivmart.ru>
2017-09-08 11:56:37 +0300

    HOLM-149: 1) Подчинил слайдер + мелкие правки

--- resources/views/Holm/footers/baseFooter.blade.php
+++ resources/views/Holm/footers/baseFooter.blade.php
@@ -4,13 +4,13 @@
             <a href="{{route('AboutPage')}}" class="footerNav__item">
                 about us
             </a>
-            <a href="{{route('ContactPage')}}" class="footerNav__item">
+            <a href="#" class="footerNav__item">
                 contact
             </a>
             <a href="{{route('FaqPage')}}" class="footerNav__item">
                 Frequently Asked Questions
             </a>
-            <a href="{{route('BlogPage')}}" class="footerNav__item">
+            <a href="#" class="footerNav__item">
                 blog
             </a>
         </div>
