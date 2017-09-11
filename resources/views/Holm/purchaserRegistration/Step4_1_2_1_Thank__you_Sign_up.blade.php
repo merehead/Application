4a44413d4ed58d96150e5fa6e5b710d8cbdf0ec8
Evan Likhvar <ivan.likhvar@gmail.com>
2017-09-11 10:14:55 +0300

    2017/09/11-2

--- resources/views/Holm/purchaserRegistration/Step4_1_2_1_Thank__you_Sign_up.blade.php
+++ resources/views/Holm/purchaserRegistration/Step4_1_2_1_Thank__you_Sign_up.blade.php
@@ -1,52 +1,6 @@
-
-{{--<div class="registration">
-    <div class="registration__column registration__column--with-padding">
-        <div class="questionsBox ">
-            <h2> profile photo </h2>
-
-
-            <div class="questionsBox__img">
-                <img src="/img/Signup_C_step16.jpg" alt="">
-            </div>
-
-
-        </div>
-
-    </div>
-    <div class="registration__column  registration__column--bg">
-        <div class="personal">
-            <form class="questionForm">
-                <div class="addRow addRow__for-single">
-                    <div class="formField">
-                        <h2 class=" formLabel questionForm__label">
-                            Please add a photo of [Service User Name]. This will only be shared with carers you choose to book and will be visible on [service user name]'s profile. You can upload a photo later if you don't have one handy.                        </h2>
-                        <div class="addContainer ">
-                            <a href="#" class="add add--moreHeight">
-                                <i class="fa fa-plus-circle"></i>
-                                <div class="add__comment">
-                                    <p>Choose a File or Drag Here</p>
-                                    <span>Size limit: 10 MB</span>
-                                </div>
-                            </a>
-                        </div>
-
-                    </div>
-                </div>
-
-
-            </form>
-
-            <form id="step" method="POST" action="{{ route('PurchaserRegistrationPost') }}">
-                {{ csrf_field() }}
-                <input type="hidden" name="step" value = '4_1_2'>
-                <input type="hidden" name="purchasersProfileID" value = {{$purchasersProfileID}}>
-            </form>
-
-        </div>
-
-    </div>
-</div>--}}
-
+{{--
+2017-09-11
+--}}
 
 <div class="thank">
     <h2 class="thank__title">
