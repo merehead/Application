379e55a8bfa3e043a583ac25873768f3f82f88e7
Evan Likhvar <ivan.likhvar@gmail.com>
2017-09-13 16:35:30 +0300

    fix postcode regex2-3

--- app/Http/Controllers/Repo/PurchaserRegistration.php
+++ app/Http/Controllers/Repo/PurchaserRegistration.php
@@ -348,7 +348,7 @@ class PurchaserRegistration
             'postcode' =>
                 array(
                     'required',
-                    'regex:#^([BMOSWbmosw][LKANlkan0-9][0-9]{1,2})|[BMOSWbmosw][LKANlkan0-9]) [0-9][A-Za-z]{1,2}$#'
+                    'regex:#^([BMOSWbmosw][LKANlkan0-9][0-9]{1,2})|([BMOSWbmosw][LKANlkan0-9]) [0-9][A-Za-z]{1,2}$#'
 //                    'regex:#^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([AZa-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))
 //[0-9][A-Za-z]{2})$#',
                 )
