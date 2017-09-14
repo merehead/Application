79417ab411d05b14daf8fe620a4e0556c89aad9c
Evan Likhvar <ivan.likhvar@gmail.com>
2017-09-14 16:32:23 +0300

    fix

--- resources/views/Holm/carerRegistration/Step13_carerRegistration.blade.php
+++ resources/views/Holm/carerRegistration/Step13_carerRegistration.blade.php
@@ -40,7 +40,7 @@
                                         <strong>{{ $errors->first('languages') }}</strong>
                                     </span>
                             @endif
-                        <div class="formField">
+                        <div class="formField hiding" style="display: none">
                             <h2 class="formLabel questionForm__label">
                                 If other, please state
                             </h2>
