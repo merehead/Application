9f07e3f9fce6a13d31f337c26229c03ce9c5d45f
Evan Likhvar <ivan.likhvar@gmail.com>
2017-09-14 11:02:05 +0300

    homework++

--- database/migrations/2017_09_12_205835_add_fields_to_carers_profiles.php
+++ database/migrations/2017_09_12_205835_add_fields_to_carers_profiles.php
@@ -17,7 +17,7 @@ class AddFieldsToCarersProfiles extends Migration
             $table->integer('paid');
             $table->dateTime('driver_licence_valid_until')->nullable();
             $table->dateTime('car_insurance_valid_until')->nullable();
-            $table->string('sort_code',16)->nullable();
+            //$table->string('sort_code',16)->nullable();
             $table->string('account_number',32)->nullable();
         });
     }
