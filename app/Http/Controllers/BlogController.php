4cba525619a8d2481f6a308de1a530cece233b77
AntonSh <anton.shelehvost@gmail.com>
2017-09-15 03:12:03 +0300

    HOLM-161 - Добавил поиск, шаринг постов, фильтр, похожие посты

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Blog;
 use Illuminate\Http\Request;
+use App\Blog;
+
 
 class BlogController extends FrontController
 {
@@ -14,31 +15,67 @@ class BlogController extends FrontController
         $this->template = config('settings.frontTheme').'.templates.homePage';
     }
 
-    public function index(){
+    public function index(Request $request){
 
+        $data = [];
         $this->title = 'Holm Care - Blog';
-
+        $input = $request->all();
         $header = view(config('settings.frontTheme').'.headers.baseHeader')->render();
         $footer = view(config('settings.frontTheme').'.footers.baseFooter')->render();
         $modals = view(config('settings.frontTheme').'.includes.modals')->render();
 
-        $blog = Blog::with('user')->get();
-        $blogDate = Blog::selectRaw("date_format(`created_at`,'%M %Y') as cdate")->groupBy('created_at')->get();
+        if(!isset($input['search']))
+            $blog = Blog::with('user')->paginate(8);
+        else
+            $blog = Blog::with('user')->where('body','like','%'.$input['search'].'%')->paginate(8);
+//dd($blog->items());
+        $blogDate = Blog::selectRaw("date_format(`created_at`,'%M %Y') as cdate,date_format(`created_at`,'%m')as month,date_format(`created_at`,'%Y')as year")->groupBy('created_at')->get();
 
+        $data = array_add($data,'blog',$blog);
+        $data = array_add($data,'blogDate',$blogDate);
 
         $this->vars = array_add($this->vars,'header',$header);
         $this->vars = array_add($this->vars,'footer',$footer);
         $this->vars = array_add($this->vars,'modals',$modals);
 
 
-        $this->content = view(config('settings.frontTheme').'.homePage.blogPage',['blog'=>$blog,'blogDate'=>$blogDate])
-            ->render();
+        $this->content = view(config('settings.frontTheme').'.homePage.blogPage',$data)->render();
+
+        return $this->renderOutput();
+    }
+
+    public function viewFilter(Request $request,$month,$year){
+        $input = $request->all();
+
+        $data = [];
+        $this->title = 'Holm Care - Blog';
+
+        $header = view(config('settings.frontTheme').'.headers.baseHeader')->render();
+        $footer = view(config('settings.frontTheme').'.footers.baseFooter')->render();
+        $modals = view(config('settings.frontTheme').'.includes.modals')->render();
+        if(!isset($input['search']))
+        $blog = Blog::with('user')->whereMonth('created_at','=',$month)->whereYear('created_at','=',$year)
+            ->paginate(8);
+        else
+            $blog = Blog::with('user')->whereMonth('created_at','=',$month)->whereYear('created_at','=',$year)->
+                where('body','like','%'.$input['search'].'%')->paginate(8);
+        $blogDate = Blog::selectRaw("date_format(`created_at`,'%M %Y') as cdate,date_format(`created_at`,'%m')as month,date_format(`created_at`,'%Y')as year")->groupBy('created_at')->get();
+
+        $data = array_add($data,'blog',$blog);
+        $data = array_add($data,'blogDate',$blogDate);
+
+        $this->vars = array_add($this->vars,'header',$header);
+        $this->vars = array_add($this->vars,'footer',$footer);
+        $this->vars = array_add($this->vars,'modals',$modals);
+
+
+        $this->content = view(config('settings.frontTheme').'.homePage.blogPage',$data)->render();
 
         return $this->renderOutput();
     }
 
     public function view(Request $request, $blog_id){
-
+        $data = [];
         $this->title = 'Holm Care - Blog';
 
         $header = view(config('settings.frontTheme').'.headers.baseHeader')->render();
@@ -46,15 +83,20 @@ class BlogController extends FrontController
         $modals = view(config('settings.frontTheme').'.includes.modals')->render();
 
         $blog = Blog::with('user')->findOrFail($blog_id);
-
-//        dd($blog);
+        $relationPost = Blog::all()->where('id','!=',$blog_id)->random(2);
+        //dd($relationPost);
+        $data = array_add($data,'relationPost',$relationPost);
+        $data = array_add($data,'blog',$blog);
+        $data = array_add($data,'image','');
+        $data = array_add($data,'title',$blog->title);
+        $data = array_add($data,'url',route('BlogPage').'/'.$blog->id);
 
         $this->vars = array_add($this->vars,'header',$header);
         $this->vars = array_add($this->vars,'footer',$footer);
         $this->vars = array_add($this->vars,'modals',$modals);
 
 
-        $this->content = view(config('settings.frontTheme').'.homePage.blogViewPage',['blog'=>$blog])->render();
+        $this->content = view(config('settings.frontTheme').'.homePage.blogViewPage',$data)->render();
 
         return $this->renderOutput();
     }
--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,33 @@
 var $carer_profile = null;
 
 // ------ Global Functions ----------
+function insertParam(key, value) {
+    key = encodeURI(key);
+    value = encodeURI(value);
+
+    var kvp = document.location.search.substr(1).split('&');
+
+    var i = kvp.length;
+    var x;
+    while (i--) {
+        x = kvp[i].split('=');
+
+        if (x[0] == key) {
+            x[1] = value;
+            kvp[i] = x.join('=');
+            break;
+        }
+    }
+    kvp = kvp.filter(function (n) {
+        return n != ''
+    });
+    if (i < 0) {
+        kvp[kvp.length] = [key, value].join('=');
+    }
+    //this will reload the page, it's likely better to store this until finished
+    document.location.search = kvp.join('&');
+}
+
 function login_ajax(form) {
     $('.login__body').hide();
     $('.loader').show();
@@ -509,5 +536,9 @@ $(document).ready(function () {
           })
         }
       }
+    });
+    
+    $('.searchContainer__input').on('change',function (e) {
+        insertParam('search',$(this).val());
     })
 });
