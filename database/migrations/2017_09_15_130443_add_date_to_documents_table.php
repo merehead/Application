ba3bec816c6f7acfb4050dd02851c501fab068cd
Andrii Ovcharuk <ovch2011@gmail.com>
2017-09-15 16:08:39 +0300

    working on documents

---
+++ database/migrations/2017_09_15_130443_add_date_to_documents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDateToDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('documents', function($table) {
+            $table->timestamp('date')->nullable()->default(null);
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('documents', function($table) {
+            $table->dropColumn('date');
+        });
+    }
+}
