692cbe54e7f1442049852b4d2b18d1ec108bf422
Andrii Ovcharuk <ovch2011@gmail.com>
2017-09-16 18:12:08 +0300

    added upload avatars feature

--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,8 @@ Route::get('/addServiceUser/','AddServiceUserController@create')->name('ServiceU
 
 Route::post('/document/upload','DocumentsController@upload')->name('UploadDocument');
 Route::get('/documents','DocumentsController@GetDocuments')->name('GetDocuments');
+Route::post('/profile-photo','ProfilePhotosController@uploadUserProfilePhoto');
+Route::post('/service-user-profile-photo','ProfilePhotosController@uploadServiceUserProfilePhoto');
 
 Route::group(['prefix' => 'admin','middleware'=> 'auth','namespace' => 'Admin'],function() {
 
