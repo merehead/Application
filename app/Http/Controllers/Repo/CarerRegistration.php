d53f517ebb1cdb7760ef8a8f9c71a144fd6b5438
Evan Likhvar <ivan.likhvar@gmail.com>
2017-09-20 17:31:02 +0300

    wrgerg

--- app/Http/Controllers/Repo/CarerRegistration.php
+++ app/Http/Controllers/Repo/CarerRegistration.php
@@ -266,6 +266,7 @@ class CarerRegistration
             'postcode' =>
                 array(
                     'required',
+
                     'regex:/^(([Bb][Ll][0-9])|([Mm][0-9]{1,2})|([Oo][Ll][0-9]{1,2})|([Ss][Kk][0-9]{1,2})|([Ww][AaNn][0-9]{1,2}) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2})$/',
                     //'regex:/^(([gG][iI][rR] {0,}0[aA]{2})|(([aA][sS][cC][nN]|[sS][tT][hH][lL]|[tT][dD][c‌​C][uU]|[bB][bB][nN][‌​dD]|[bB][iI][qQ][qQ]‌​|[fF][iI][qQ][qQ]|[p‌​P][cC][rR][nN]|[sS][‌​iI][qQ][qQ]|[iT][kK]‌​[cC][aA]) {0,}1[zZ]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yxA-HK-XY]?[0-9][‌​0-9]?)|(([a-pr-uwyzA‌​-PR-UWYZ][0-9][a-hjk‌​stuwA-HJKSTUW])|([a-‌​pr-uwyzA-PR-UWYZ][a-‌​hk-yA-HK-Y][0-9][abe‌​hmnprv-yABEHMNPRV-Y]‌​))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$/', //[0-9][A-Za-z]{1,2}
 
