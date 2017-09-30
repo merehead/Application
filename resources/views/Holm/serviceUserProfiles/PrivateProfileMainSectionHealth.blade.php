65280dc5acff583ad71140431b628120e2c287bf
evan <elk@gmail.com>
2017-09-30 21:13:23 +0300

    461

--- resources/views/Holm/serviceUserProfiles/PrivateProfileMainSectionHealth.blade.php
+++ resources/views/Holm/serviceUserProfiles/PrivateProfileMainSectionHealth.blade.php
@@ -46,13 +46,13 @@
                 <span class="help-block"><strong>{{ $errors->first('comprehension') }}</strong></span>
             @endif
         </div>
-        <div class="profileField profileField--two-thirds"{!!  ($serviceUsersProfile->comprehension == 'No' || is_null($serviceUsersProfile->comprehension) )? ' style="display:none"' : ''!!}>
+{{--        <div class="profileField profileField--two-thirds"{!!  ($serviceUsersProfile->comprehension == 'No' || is_null($serviceUsersProfile->comprehension) )? ' style="display:none"' : ''!!}>
             <h2 class="profileField__title ordinaryTitle"><span class="ordinaryTitle__text ordinaryTitle__text--smaller">Please, give details  </span></h2>
             {!! Form::text('comprehension_detail',null,['class'=>'profileField__input ','placeholder'=>'Type details','maxlength'=>"250"]) !!}
             @if ($errors->has('comprehension_detail'))
                 <span class="help-block"><strong>{{ $errors->first('comprehension_detail') }}</strong></span>
             @endif
-        </div>
+        </div>--}}
     </div>
     <div class="profileRow">
         <div class="profileField">
@@ -67,7 +67,7 @@
         <div class="profileField profileField--two-thirds"{!!  ($serviceUsersProfile->communication == 'No' || is_null($serviceUsersProfile->communication) )? ' style="display:none"' : ''!!}>
             <h2 class="profileField__title ordinaryTitle"><span class="ordinaryTitle__text ordinaryTitle__text--smaller">Please, give details  </span></h2>
             {!! Form::text('common_communication_details',null,['class'=>'profileField__input ','placeholder'=>'Type details','maxlength'=>"250"]) !!}
-            @if ($errors->has('common_communication_details'))
+            @if ($errors->has('comprehension_detail'))
                 <span class="help-block"><strong>{{ $errors->first('common_communication_details') }}</strong></span>
             @endif
         </div>
