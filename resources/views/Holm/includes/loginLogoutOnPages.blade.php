962e2feecf6395feee38482b29531c493f5bf195
Evan Likhvar <ivan.likhvar@gmail.com>
2017-10-03 12:49:23 +0300

    mobile_menu

--- resources/views/Holm/includes/loginLogoutOnPages.blade.php
+++ resources/views/Holm/includes/loginLogoutOnPages.blade.php
@@ -107,21 +107,13 @@
 
 @else
 
-
-
-    <div class="loginBox">
-        <a href="/login" class=" centeredLink loginBox__link"
-           data-toggle="modal" data-target="#login"
-                {{--onclick="event.preventDefault();document.getElementById('login').style.display = 'block';"--}}
-        >
+    {{--<div class="loginBox">--}}
+        <a href="/login" class=" centeredLink loginBox__link" data-toggle="modal" data-target="#login">
             Login
         </a>
-        <a href="/" id="sign_up_button" class=" centeredLink loginBox__link loginBox__link--active"
-           data-toggle="modal" data-target="#signUpdiv"
-                {{--onclick="event.preventDefault();document.getElementById('sign_up_div').style.display = 'block';"--}}
-        >
+        <a href="/" id="sign_up_button" class=" centeredLink loginBox__link loginBox__link--active" data-toggle="modal" data-target="#signUpdiv">
             Sign up
         </a>
-    </div>
+    {{--</div>--}}
 
 @endif
