d5867a9a14efce8697b881fec91a7cf9008f5323
Evan Likhvar <ivan.likhvar@gmail.com>
2017-10-04 20:41:36 +0300

    contact mail 494

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -52,7 +52,7 @@ class ContactController extends FrontController
     public function send(Request $request){
 
         $input = $request->all();
-        $message = '';
+/*        $message = '';
         $message .= 'My Name:'. $input['name'].PHP_EOL;
         $message .= 'My email:'. $input['email'].PHP_EOL;
         $message .= 'My phone:'. $input['phone'].PHP_EOL;
@@ -61,9 +61,30 @@ class ContactController extends FrontController
             'body'=> $message.'<br/><br/> Message:'.$input['message']
         ];
 
+
         $receiverAddress = 'info@holm.care';
 
-        Mail::to($receiverAddress)->send(new OrderShipped($content));
+        Mail::to($receiverAddress)->send(new OrderShipped($content));*/
+
+        try {
+
+            Mail::send(config('settings.frontTheme') . '.emails.contact',
+                ['userMessage'=>$input['message'],'phone'=>$input['phone'],'email'=>$input['email'],'userName'=>$input['name']],
+                function ($m) use ($input) {
+                    $m->from($input['email'])->to('info@holm.care')->subject($input['topic']);
+                });
+        }
+        catch (Swift_TransportException $STe){
+
+            $error = MailError::create([
+                'error_message'=>$STe->getMessage(),
+                'function'=>__METHOD__,
+                'action'=>'Try to sent contact form',
+                //'user_id'=>$user->id
+            ]);
+        }
+
+
 
         return redirect()->route('ThankPage');
 
