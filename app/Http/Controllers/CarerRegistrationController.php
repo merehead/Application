5aa1c0d194cb7b1d5fb2648883c9c021c4dd24e2
Evan Likhvar <ivan.likhvar@gmail.com>
2017-10-05 14:38:01 +0300

    tmp

--- app/Http/Controllers/CarerRegistrationController.php
+++ app/Http/Controllers/CarerRegistrationController.php
@@ -136,4 +136,25 @@ class CarerRegistrationController extends FrontController
 
     }
 
+    public function sendContinueRegistration(){
+
+
+        $this->title = 'Carer Registration';
+
+        $header = view(config('settings.frontTheme').'.headers.baseHeader')->render();
+        $footer = view(config('settings.frontTheme').'.footers.baseFooter')->render();
+        $modals = view(config('settings.frontTheme').'.includes.modals')->render();
+
+        $this->vars = array_add($this->vars,'header',$header);
+        $this->vars = array_add($this->vars,'footer',$footer);
+        $this->vars = array_add($this->vars,'modals',$modals);
+
+        $user = Auth::user();
+
+
+        $this->content = view(config('settings.frontTheme').'.carerRegistration.thankYou')->render();
+
+        return $this->renderOutput();
+    }
+
 }
