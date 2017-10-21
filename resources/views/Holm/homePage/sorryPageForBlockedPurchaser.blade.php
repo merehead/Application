1f87c363a01c3476ce91becf132e6795254a6e83
AntonShelehvost <anton.shelehvost@gmail.com>
2017-10-21 23:06:41 +0300

    HOLM-711 Page design Sorry for the search

--- resources/views/Holm/homePage/sorryPageForBlockedPurchaser.blade.php
+++ resources/views/Holm/homePage/sorryPageForBlockedPurchaser.blade.php
@@ -1,18 +1,24 @@
-<section class="mainSection">
+<section class="mainSection" style="padding-bottom: 126px;">
     <div class="container">
+
         <div class="sorry-page">
-        <span class="sorry-page__ico">
-          <i class="fa fa-ban" aria-hidden="true"></i>
-        </span>
+          <span class="sorry-page__ico">
+            <i class="fa fa-ban" aria-hidden="true"></i>
+          </span>
             <h2 class="sorry-page__title">
                 Sorry!
             </h2>
+
             <p class="info-p info-p--roboto ">
                 Your account was blocked by admin.
             </p>
             <p class="info-p info-p--roboto">
                 If you feel we should reconsider this decision, please do feel free to contact us via info@holm.care
             </p>
+
         </div>
     </div>
 </section>
+
+
+
