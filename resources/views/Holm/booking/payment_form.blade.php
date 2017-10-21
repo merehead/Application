586c05c85c6b05995fd426eae80ec0edbc50b8e8
AntonShelehvost <anton.shelehvost@gmail.com>
2017-10-21 21:05:00 +0300

    HOLM-792 edit html form bookings purchase

--- resources/views/Holm/booking/payment_form.blade.php
+++ resources/views/Holm/booking/payment_form.blade.php
@@ -4,15 +4,12 @@
             <a href="/" class="breadcrumbs__item">
                 Home
             </a>
-            <span class="breadcrumbs__arrow">></span>
+            <span class="breadcrumbs__arrow">&gt;</span>
             <a href="/serviceUser-settings/booking/{{$serviceUser->id}}" class="breadcrumbs__item">
                 Booking Payment
             </a>
-
         </div>
-
         <div class="bookPaymentWrap">
-            {{--@if($booking->payment_method == null)--}}
             <h2 class="paymentDetails">
                 Payment details
             </h2>
@@ -26,90 +23,99 @@
                             Bonus wallet
                         </a>
                     </div>
-                    <div class="bookPayment__form-header">
-                          <div class="formField">
-                              <h2 class="formLabel">
-                                  Card owner's name
-                              </h2>
-                              <div class="inputWrap">
-                                  <input type="text" class="formInput" placeholder="Cris Jones">
-                              </div>
-                          </div>
-                          <div class="bookPayment__field">
-                              <h2 class="formLabel">
-                                  Card Number
-                              </h2>
-                              <div class="inputWrap">
-                                  <input type="text" class="formInput" id="cardNumber" placeholder="4534 3333 3333 3333 3333">
-                    <span class="bookPayment__ico">
-                      <img src={{asset("img/visa.png")}} alt="">
-                    </span>
-                              </div>
-                          </div>
-                          <div class="bookPayment__row">
-                              <div class="bookPayment__halfColumn">
-                                  <div class="formField">
-                                      <h2 class="formLabel">
-                                          Valid Until
-                                      </h2>
-                                      <div class="inputWrap">
-                                          <input type="text" class="formInput" id="cardMonth" placeholder="MM">
-                                          <input type="text" class="formInput" id="cardYear" placeholder="YY">
-                                      </div>
-                                  </div>
-                              </div>
-                              <div class="bookPayment__halfColumn">
-                                  <div class="formField">
-                                      <h2 class="formLabel">
-                                          cvc code
-                                      </h2>
-                                      <div class="inputWrap">
-                                          <input type="text" class="formInput" id="cardCVC" placeholder="202">
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                          <div class="paymentCheckbox">
-                              <div class="checkBox_item">
-                              </div>
-                          </div>
-                          <div class="roundedBtn roundedBtn--center">
-                              <button  class="roundedBtn__item roundedBtn__item--confirm" id="buttonPaymentCard">
-                                  Confirm Payment
-                              </button>
-                          </div>
-                    </div>
+                    <form action="" class="bookPayment__form bookPayment__form-header">
+                        <div class="formField">
+                            <h2 class="formLabel">
+                                Card owner's name
+                            </h2>
+                            <div class="inputWrap">
+                                <input type="text" class="formInput" placeholder="Cris Jones">
+                            </div>
+                        </div>
+                        <div class="bookPayment__field">
+                            <h2 class="formLabel">
+                                Card Number
+                            </h2>
+                            <div class="inputWrap">
+                                <input type="text" class="formInput" id="cardNumber" placeholder="4534 3333 3333 3333 3333">
+                                <span class="bookPayment__ico">
+                        <img src="{{asset("img/visa.png")}}" alt="">
+                      </span>
+                            </div>
+                        </div>
+                        <div class="bookPayment__row bookPayment__row--xs-column">
+                            <div class="bookPayment__halfColumn   bookPayment__halfColumn--xs-full">
+                                <h2 class="formLabel">
+                                    Valid Until
+                                </h2>
+                                <div class="bookPayment__row">
+                                    <div class="bookPayment__halfColumn">
+                                        <div class="formField">
+                                            <div class="inputWrap">
+                                                <input type="text" id="cardMonth" class="formInput" placeholder="MM">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="bookPayment__halfColumn">
+                                        <div class="formField">
+                                            <div class="inputWrap">
+                                                <input type="text" id="cardYear" class="formInput" placeholder="YY">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
 
+                            </div>
+                            <div class="bookPayment__halfColumn bookPayment__halfColumn--xs-full">
+                                <div class="formField">
+                                    <h2 class="formLabel">
+                                        cvc code
+                                    </h2>
+                                    <div class="inputWrap">
+                                        <input type="text" id="cardCVC" class="formInput" placeholder="202">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="paymentCheckbox">
+                            <div class="checkBox_item">
+                                <input type="checkbox" name="checkbox" class="customCheckbox" id="buttonPaymentCard1">
+                                <label for="buttonPaymentCard1"> save payment details?</label>
+                            </div>
+                        </div>
+                        <div class="roundedBtn roundedBtn--center">
+                            <a href="#" class="roundedBtn__item roundedBtn__item--confirm" id="buttonPaymentCard">
+                                Confirm Payment
+                            </a>
+                        </div>
+                    </form>
                     <div class="bonusPay-header" style="display: none">
-                      <div class="bonusPay">
-                       <div class="bonusPay__item">
-                         <p>
-                           Bonuses balance
-                         </p>
-                         <p>
-                           Bonuses balance after payment
-                         </p>
-                       </div>
-                       <div class="bonusPay__item bonusPay__item--border">
+                        <div class="bonusPay">
+                            <div class="bonusPay__item">
+                                <p>
+                                    Bonuses balance
+                                </p>
+                                <p>
+                                    Bonuses balance after payment
+                                </p>
+                            </div>
+                            <div class="bonusPay__item bonusPay__item--border">
                          <span class="bonusPay__label">
                            £{{$user->bonus_balance}}
                          </span>
-                         <span class="bonusPay__label">
+                                <span class="bonusPay__label">
                            £{{$user->bonus_balance - $booking->price}}
                          </span>
-                       </div>
+                            </div>
 
-                     </div>
-                     <div class="roundedBtn roundedBtn--center">
-                       <button  class="roundedBtn__item roundedBtn__item--confirm" id="buttonPaymentBonuses">
-                         Confirm Payment
-                       </button>
-                     </div>
+                        </div>
+                        <div class="roundedBtn roundedBtn--center">
+                            <button  class="roundedBtn__item roundedBtn__item--confirm" id="buttonPaymentBonuses">
+                                Confirm Payment
+                            </button>
+                        </div>
                     </div>
-
                 </div>
-
-
                 <div class="bookPayment__total">
                     <div class="paymentTotal">
                         <div class="paymentTotal__item">
@@ -143,7 +149,6 @@
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 </section>
