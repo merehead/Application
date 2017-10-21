bf5b305fa36c8d7421f6459cacb5061e98891822
AntonShelehvost <anton.shelehvost@gmail.com>
2017-10-21 22:49:03 +0300

    HOLM-786 Redesign of the section Purchaser Private Profile

--- resources/views/Holm/purchaserProfiles/PrivateProfileMainSectionPaymentDetail.blade.php
+++ resources/views/Holm/purchaserProfiles/PrivateProfileMainSectionPaymentDetail.blade.php
@@ -5,75 +5,48 @@
         <button type="button" class="btn btn-success hidden" id="load" data-loading-text="<i class='fa fa-spinner
         fa-spin '></i> Processing"><i class="fa fa-floppy-o"></i>  Save</button>
     </div>
-</div>
-<div class="borderContainer">
-{{--    <h2 class="fieldCategory">
-        Payments Details
-    </h2>--}}
 
     {!! Form::model($purchaserProfile, ['method'=>'POST','route'=>'purchaserSettingsPost','id'=>'Payment']) !!}
     {!! Form::hidden('id',null) !!}
     {!! Form::hidden('stage','payment') !!}
-
-
-    <div class="profileRow">
-
-        <div class="profileField">
-            <h2 class="profileField__title ordinaryTitle">
-              <span class="ordinaryTitle__text ordinaryTitle__text--smaller">
-                CARDHOLDER'S NAME <span class="requireIco">*</span>
-              </span>
-            </h2>
-            {!! Form::text('name_of_cardholder',null,['class'=>'profileField__input','placeholder'=>'CARDHOLDER\'S NAME']) !!}
-
-        </div>
-
-        <div class="profileField">
-            <h2 class="profileField__title ordinaryTitle">
-              <span class="ordinaryTitle__text ordinaryTitle__text--smaller">
-                PAYMENT CARD NUMBER <span class="requireIco">*</span>
-              </span>
-            </h2>
-            {!! Form::text('payment_card_number',null,['class'=>'profileField__input','placeholder'=>'PAYMENT CARD NUMBER']) !!}
-
-        </div>
-
-
-        <div class="profileField">
-            <h2 class="profileField__title ordinaryTitle">
-              <span class="ordinaryTitle__text ordinaryTitle__text--smaller">
-                EXPIRY DATE <span class="requireIco">*</span>
-              </span>
-            </h2>
-            {!! Form::text('expiry_date',null,['class'=>'profileField__input','placeholder'=>'EXPIRY DATE']) !!}
-
-
-        </div>
-
-
-    </div>
-
     <div class="profileRow">
         <div class="profileField">
             <h2 class="profileField__title ordinaryTitle">
-              <span class="ordinaryTitle__text ordinaryTitle__text--smaller">
-                cvv <span class="requireIco">*</span>
-              </span>
+                <span class="ordinaryTitle__text ordinaryTitle__text--smaller">
+                  cvv <span class="requireIco">*</span>
+                </span>
             </h2>
             {!! Form::text('cvv',null,['class'=>'profileField__input','placeholder'=>'CVV']) !!}
-
         </div>
+        <div class="profileField">
+            <h2 class="profileField__title ordinaryTitle">
+                <span class="ordinaryTitle__text ordinaryTitle__text--smaller">
+                  PAYMENT CARD NUMBER <span class="requireIco">*</span>
+                </span>
+            </h2>
+            {!! Form::text('payment_card_number',null,['class'=>'profileField__input','placeholder'=>'PAYMENT CARD NUMBER']) !!}
+        </div>
+
 
         <div class="profileField">
             <h2 class="profileField__title ordinaryTitle">
-              <span class="ordinaryTitle__text ordinaryTitle__text--smaller">
-                PAYPAL/AMAZON DETAILS <span class="requireIco">*</span>
-              </span>
+                <span class="ordinaryTitle__text ordinaryTitle__text--smaller">
+                  EXPIRY DATE <span class="requireIco">*</span>
+                </span>
             </h2>
-            {!! Form::text('paypal_amazon_details',null,['class'=>'profileField__input','placeholder'=>'PAYPAL/AMAZON DETAILS']) !!}
-
-
+            {!! Form::text('expiry_date',null,['class'=>'profileField__input','placeholder'=>'EXPIRY DATE']) !!}
         </div>
+    </div>
+
+    <div class="payment-control">
+        <a href="#" class="payment-control__item payment-control__item--delete">
+            <i class="fa fa-trash"></i>
+            <span>delete</span>
+        </a>
+        <a href="#" class="payment-control__item">
+            <i class="fa fa-plus"></i>
+            <span>add</span>
+        </a>
     </div>
     {!! Form::close()!!}
 </div>
