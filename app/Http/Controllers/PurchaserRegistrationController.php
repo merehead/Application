32516d8edb6206eca380d700c26fd24dd343b30b
AntonShelehvost <anton.shelehvost@gmail.com>
2017-10-27 17:45:18 +0300

    HOLM-751 Purchaser login on sign up for purchasers page

--- app/Http/Controllers/PurchaserRegistrationController.php
+++ app/Http/Controllers/PurchaserRegistrationController.php
@@ -61,7 +61,7 @@ class PurchaserRegistrationController extends FrontController
             $purchasersProfile = PurchasersProfile::findOrFail($user->id);
 
 
-            if ($purchasersProfile->registration_status == 'completed' && !$purchasersProfile->is_uncompleted_service_user) {
+            if ($purchasersProfile->registration_status == 'completed') {
                 return redirect(route('purchaserSettings'));
             }
 
