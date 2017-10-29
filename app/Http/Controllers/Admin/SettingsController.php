ed1e1443e8b735ad84073c46b624e9e1bac5a72c
AntonShelehvost <anton.shelehvost@gmail.com>
2017-10-29 20:12:08 +0300

    HOLM-23 Admin settings

--- app/Http/Controllers/Admin/SettingsController.php
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Holiday;
 use App\User;
 use DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 
 class SettingsController extends AdminController
@@ -15,29 +16,55 @@ class SettingsController extends AdminController
         $this->template = config('settings.theme') . '.templates.adminBase';
     }
 
-    public function index(){
+    public function index()
+    {
 
-        $data = User::get()->where('user_type_id','=','4');
-        $this->vars['users']=$data;
-        $this->content = view(config('settings.theme').'.settings')->with( $this->vars)->render();
+        $data = User::get()->where('user_type_id', '=', '4');
+        $this->vars['users'] = $data;
+        $this->content = view(config('settings.theme') . '.settings')->with($this->vars)->render();
 
         return $this->renderOutput();
     }
 
-    public function update(Request $request){
+    public function update(Request $request)
+    {
         $input = $request->all();
-        switch ($input['type']){
+        switch ($input['type']) {
             case 'password':
                 $user_id = $input['id'];
                 $obj_user = User::find($user_id)->first();
                 $obj_user->password = Hash::make($input['newPassword']);
                 $obj_user->save();
-
-                return response()->json(["result"=>true]);
+                return response()->json(["result" => true]);
                 break;
-        }
-        if($input['type']){
+            case 'invite':
+                try {
+                    if(User::where('email','=',$input['email'])->count()==0) {
+                        $password = str_random(8);
+                        $user = new User();
+                        $user->password = Hash::make($password);
+                        $user->email = $input['email'];
+                        $user->user_type_id = 4;
+                        $user->save();
+                        $text = view(config('settings.frontTheme') . '.emails.invite_admins')->with([
+                            'email' => $input['email'], 'password' => $password
+                        ])->render();
 
+                        DB::table('mails')
+                            ->insert(
+                                [
+                                    'email' => $input['email'],
+                                    'subject' => 'INVITE ADMINS',
+                                    'text' => $text,
+                                    'time_to_send' => date('Y-m-d H:i:s'),
+                                    'status' => 'new'
+                                ]);
+                        return response()->json(["result" => true]);
+                    }else return response()->json(["result" => false, 'msg' => 'User is exist. To change email.']);
+                    break;
+                } catch (Exception $e) {
+                    return response()->json(["result" => false, 'msg' => ' Sorry something went worng. Please try again.']);
+                }
         }
 
         return $this->renderOutput();
--- resources/views/HolmAdmin/settings.blade.php
+++ resources/views/HolmAdmin/settings.blade.php
@@ -6,67 +6,77 @@
         admin settings
     </h2>
     <div class="settingBox">
-        <div class="settingContainer">
-            <div class="settingCard">
-                <div class="settingCard__head">
-                    <h2>General</h2>
-                </div>
-                <div class="settingProfile">
-                    <a href="#" class="settingProfile__img">
-                        <img src="/img/admin/nik.png" alt="">
-                        {{--<span class="addIco">--}}
-                    {{--<i class="fa fa-plus-circle" aria-hidden="true"></i>--}}
-
-                  {{--</span>--}}
-                    </a>
-                    <div class="settingProfile__info">
-                        <div class="formField">
-                            <h2 class="fieldLabel">First name</h2>
-                            <div class="fieldWrap">
-                                <input type="text" name="" value="NIK SETH" class="formItem formItem--without-ico" placeholder="Nik">
-                            </div>
-                        </div>
-
+        <form action="{{route('settingsAdminPost')}}" method="post" id="general">
+            <?php echo e(csrf_field()); ?>
+            <input type="hidden" name="type" value="password">
+            <input type="hidden" name="id" value="{{Auth::user()->id}}">
+            <div class="settingContainer">
+                <div class="settingCard">
+                    <div class="settingCard__head">
+                        <h2>General</h2>
                     </div>
-                </div>
-                <div class="passWrap">
-                    <div class="passBox">
-                        <div class="fieldRow">
-                            <div class="formField formField--half">
-                                <h2 class="fieldLabel">Change password</h2>
+
+                    <div class="settingProfile">
+                        <a href="#" class="settingProfile__img">
+                            <img src="/img/admin/nik.png" alt="">
+                            {{--<span class="addIco">--}}
+                            {{--<i class="fa fa-plus-circle" aria-hidden="true"></i>--}}
+
+                            {{--</span>--}}
+                        </a>
+                        <div class="settingProfile__info">
+                            <div class="formField">
+                                <h2 class="fieldLabel">First name</h2>
                                 <div class="fieldWrap">
-                                    <input type="text" class="formItem formItem--without-ico" placeholder="*******">
+                                    <input type="text" name="first_name" value="NIK SETH"
+                                           class="formItem formItem--without-ico" placeholder="Nik">
                                 </div>
                             </div>
-                            <div class="formField formField--half">
-                                <h2 class="fieldLabel">Repeat password</h2>
-                                <div class="fieldWrap">
-                                    <input type="text" class="formItem formItem--without-ico" placeholder="*********">
+
+                        </div>
+                    </div>
+                    <div class="passWrap">
+                        <div class="passBox">
+                            <div class="fieldRow">
+                                <div class="formField formField--half">
+                                    <h2 class="fieldLabel">Change password</h2>
+                                    <div class="fieldWrap">
+                                        <input type="password" name="password" class="formItem formItem--without-ico"
+                                               placeholder="*******">
+                                    </div>
+                                </div>
+                                <div class="formField formField--half">
+                                    <h2 class="fieldLabel">Repeat password</h2>
+                                    <div class="fieldWrap">
+                                        <input type="password" name="newPassword" class="formItem formItem--without-ico"
+                                               placeholder="*********">
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
+                </div>
             </div>
-        </div>
-
-        <div class="settingBtn">
-            <a href="#" class="actionsBtn actionsBtn--accept actionsBtn--big">
-                save
-            </a>
-        </div>
-
+            <div class="settingBtn">
+                <a href="#" class="actionsBtn actionsBtn--accept actionsBtn--big"
+                   onclick="event.preventDefault();saveGeneral();">
+                    save
+                </a>
+            </div>
+        </form>
     </div>
     <div class="invite">
         <h2 class="invite__title">
             invite admins
         </h2>
-        <form class="inviteForm">
+        <form class="inviteForm" id="invite-form" method="post" action="{{route('settingsAdminPost')}}">
+            <input type="hidden" name="type" value="invite">
+            <?php echo e(csrf_field()); ?>
             <div class="inviteForm__field">
-                <input type="text" class="inviteForm__input" placeholder="Email">
+                <input type="email" id="invite" name="email" class="inviteForm__input" placeholder="Email">
             </div>
-            <button class="inviteForm__btn">
+            <button class="inviteForm__btn" onclick="event.preventDefault();sendInvite();">
                 invite
             </button>
         </form>
@@ -133,40 +143,40 @@
 
                 <tbody>
                 @foreach($users as $user)
-                <tr>
+                    <tr>
 
 
-                    <td class="for-inner">
-                        <table class="innerTable ">
-                            <tbody>
-                            <tr>
-                                <td class=" ">
-                                    <span>{{$user->id}}</span>
-                                </td>
-                                <td class="">
-                                    <a href="#" class="tableLink">
-                                        {{$user->first_name.' '.$user->family_name}}
-                                    </a>
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </td>
+                        <td class="for-inner">
+                            <table class="innerTable ">
+                                <tbody>
+                                <tr>
+                                    <td class=" ">
+                                        <span>{{$user->id}}</span>
+                                    </td>
+                                    <td class="">
+                                        <a href="#" class="tableLink">
+                                            {{$user->first_name.' '.$user->family_name}}
+                                        </a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
 
-                    <td>
+                        <td>
                     <span>
                       {{$user->email}}
                     </span>
-                    </td>
-                    <td>
-                        <div class="actionsGroup">
-                            <a href="#" class="actionsBtn actionsBtn--delete  ">
-                                delete
-                            </a>
-                        </div>
-                    </td>
-                </tr>
-               @endforeach
+                        </td>
+                        <td>
+                            <div class="actionsGroup">
+                                <a href="#" class="actionsBtn actionsBtn--delete  ">
+                                    delete
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
 
 
                 </tbody>
@@ -177,7 +187,60 @@
 
 </div>
 <script>
-    $(document).on('ready',function(){
+    function sendInvite() {
+        var form = $('form#invite-form');
+        var token = $(form).find('input[name="_token"]').val();
+        console.log(form.serialize());
+        $.ajax({
+            url: $(form).attr('action'),
+            headers: {'X-CSRF-TOKEN': token},
+            data: $(form).serialize(),
+            type: 'POST',
+            dataType: "json",
+            success: function (response) {
+                if(response.result=='true') {
+                    var notify = $.notify('<strong>Saving</strong> success...', {
+                        type: 'success',
+                        allow_dismiss: true
+                    });
+                    $('#invite').val('');
+                }else{
+                    var notify = $.notify('<strong>'+response.msg+'</strong>', {
+                        type: 'danger',
+                        allow_dismiss: false
+                    });
+                }
+            },
+            error: function (response) {
+                console.log(response)
+                var notify = $.notify('<strong>'+response.msg+'</strong>', {
+                    type: 'danger',
+                    allow_dismiss: false
+                });
+            }
+        });
+    }
 
-    })
+    function saveGeneral() {
+        var form = $('form#general');
+        var token = $(form).find('input[name="_token"]').val();
+        console.log(form.serialize());
+        $.ajax({
+            url: $(form).attr('action'),
+            headers: {'X-CSRF-TOKEN': token},
+            data: $(form).serialize(),
+            type: 'POST',
+            dataType: "json",
+            success: function (response) {
+                var notify = $.notify('<strong>Saving</strong> success...', {
+                    type: 'success',
+                    allow_dismiss: true
+                });
+                $(form).reset();
+            },
+            error: function (response) {
+                console.log(response)
+            }
+        });
+    }
 </script>
