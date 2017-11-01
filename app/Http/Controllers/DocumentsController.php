15f223a3ce3982836b6505b0be392c6a61c749ce
Andrii Ovcharuk <ovch2011@gmail.com>
2017-11-01 18:25:00 +0200

    image rotating doci=uments

--- app/Http/Controllers/DocumentsController.php
+++ app/Http/Controllers/DocumentsController.php
@@ -106,6 +106,12 @@ class DocumentsController extends Controller
                 ]);
             }
 
+            if(in_array($file->extension(), ['png', 'jpg', 'jpeg'])){
+                $image = new \Imagick($fileName);
+                $this->autoRotateImage($fileName);
+                $image->writeImage($fileName);
+            }
+
             $file->move(storage_path() . '/documents/', $fileName);
 
             return ['id' => $document->id, 'type' => $request->type];
@@ -138,5 +144,26 @@ class DocumentsController extends Controller
             unlink(storage_path().'/documents/'.$document->file_name);
         $document->delete();
         return response(['status' => 'success']);
+    }
+
+    private function autoRotateImage($image) {
+        $orientation = $image->getImageOrientation();
+
+        switch($orientation) {
+            case \imagick::ORIENTATION_BOTTOMRIGHT:
+                $image->rotateimage("#000", 180); // rotate 180 degrees
+                break;
+
+            case \imagick::ORIENTATION_RIGHTTOP:
+                $image->rotateimage("#000", 90); // rotate 90 degrees CW
+                break;
+
+            case \imagick::ORIENTATION_LEFTBOTTOM:
+                $image->rotateimage("#000", -90); // rotate 90 degrees CCW
+                break;
+        }
+
+        // Now that it's auto-rotated, make sure the EXIF data is correct in case the EXIF gets saved with the image!
+        $image->setImageOrientation(\imagick::ORIENTATION_TOPLEFT);
     }
 }
