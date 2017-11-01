268bc7b9701d8527cd6c05130c8614c02ffed2a6
Andrii Ovcharuk <ovch2011@gmail.com>
2017-11-01 18:07:05 +0200

    image rotating

--- app/Http/Controllers/ProfilePhotosController.php
+++ app/Http/Controllers/ProfilePhotosController.php
@@ -10,11 +10,14 @@ use Auth;
 class ProfilePhotosController extends Controller
 {
     public function uploadUserProfilePhoto(Request $request){
-        $user = Auth::user();
-//        $user = User::find(1);
+//        $user = Auth::user();
+        $user = User::find(1);
         $base64 = $request->image;
         $data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64));
         file_put_contents('img/profile_photos/'.$user->id.'.png', $data);
+        $image = new \Imagick('img/profile_photos/'.$user->id.'.png');
+        $this->autoRotateImage($image);
+        $image->writeImage('img/profile_photos/'.$user->id.'.png');
         return response(['status' => 'success']);
     }
 
@@ -23,6 +26,30 @@ class ProfilePhotosController extends Controller
         $base64 = $request->image;
         $data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64));
         file_put_contents('img/service_user_profile_photos/'.$serviceUser->id.'.png', $data);
+        $image = new \Imagick('img/service_user_profile_photos/'.$serviceUser->id.'.png');
+        $this->autoRotateImage($image);
+        $image->writeImage('img/service_user_profile_photos/'.$serviceUser->id.'.png');
         return response(['status' => 'success']);
+    }
+
+    private function autoRotateImage($image) {
+        $orientation = $image->getImageOrientation();
+
+        switch($orientation) {
+            case \imagick::ORIENTATION_BOTTOMRIGHT:
+                $image->rotateimage("#000", 180); // rotate 180 degrees
+                break;
+
+            case \imagick::ORIENTATION_RIGHTTOP:
+                $image->rotateimage("#000", 90); // rotate 90 degrees CW
+                break;
+
+            case \imagick::ORIENTATION_LEFTBOTTOM:
+                $image->rotateimage("#000", -90); // rotate 90 degrees CCW
+                break;
+        }
+
+        // Now that it's auto-rotated, make sure the EXIF data is correct in case the EXIF gets saved with the image!
+        $image->setImageOrientation(\imagick::ORIENTATION_TOPLEFT);
     }
 }
