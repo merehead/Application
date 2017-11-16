52da52fb9e92dede877f1e57203d4d468a14c863
AntonShelehvost <anton.shelehvost@gmail.com>
2017-11-16 17:35:51 +0200

    HOLM-939 offer an unsubscribe option on our marketing emails

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,4 +26,9 @@ class HomeController extends Controller
         //return view('home');
         return redirect('/admin');
     }
+
+    public function  unsubscribe($id){
+
+        return redirect('/');
+    }
 }
