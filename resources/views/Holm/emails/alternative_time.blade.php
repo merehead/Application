8653be08cfd9355d56e64b864e8ab05ddcafd9b5
AntonShelehvost <anton.shelehvost@gmail.com>
2017-12-04 15:38:09 +0200

    HOLM-1026 Email error - Carer offered alternative time

--- resources/views/Holm/emails/alternative_time.blade.php
+++ resources/views/Holm/emails/alternative_time.blade.php
@@ -7,35 +7,65 @@
           integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900|Open+Sans:400,600,700|Roboto:300,400,400i,500,700"
           rel="stylesheet">
-    <style type="text/css"> a:hover {  text-decoration: none !important;  }  a {  color: #71bc37;  text-decoration: none;  }  @media screen and (max-width: 768px) {  .logo {  float: none;  display: block;  }  }  .title {  padding: 15px;  }</style>
+    <style type="text/css"> a:hover {
+            text-decoration: none !important;
+        }
+
+        a {
+            color: #71bc37;
+            text-decoration: none;
+        }
+
+        @media screen and (max-width: 768px) {
+            .logo {
+                float: none;
+                display: block;
+            }
+        }
+
+        .title {
+            padding: 15px;
+        }</style>
 </head>
 <body style="margin: 0; background: #fff; font-size:18px">
 <table cellpadding="0" cellspacing="0" border="0" align="center" style="margin-top: 30px;
       border-collapse: collapse;
       -webkit-box-shadow: 0px 0px 101px 0px rgba(31, 31, 33, 0.15);
-      box-shadow: 0px 0px 101px 0px rgba(31, 31, 33, 0.15); width: 100%; max-width: 940px; " bgcolor="#ffffff" class="container">
+      box-shadow: 0px 0px 101px 0px rgba(31, 31, 33, 0.15); width: 100%; max-width: 940px; " bgcolor="#ffffff"
+       class="container">
     <tr>
         <td valign="top" align="left" bgcolor="#ffffff" style="padding-bottom: 40px;">
-            <table cellpadding="0" cellspacing="0" border="0" style=" border-collapse: collapse;background-size: 100%;   margin: 0; padding: 10px 30px;" width="100%" class="content">
+            <table cellpadding="0" cellspacing="0" border="0"
+                   style=" border-collapse: collapse;background-size: 100%;   margin: 0; padding: 10px 30px;"
+                   width="100%" class="content">
                 <tr>
-                    <td style="padding-left: 40px;" align="left" valign="middle"><img src="{{asset('img/logo.png')}}" alt="" class="logo" style="width: 120px; float: left; "></td>
-                    <td style="padding-left: 30px;"><img src="{{asset('img/l6.png')}}" alt="" class="" style="width:  100% ; float: right; "></td>
+                    <td style="padding-left: 40px;" align="left" valign="middle"><img src="{{asset('img/logo.png')}}"
+                                                                                      alt="" class="logo"
+                                                                                      style="width: 120px; float: left; ">
+                    </td>
+                    <td style="padding-left: 30px;"><img src="{{asset('img/l6.png')}}" alt="" class=""
+                                                         style="width:  100% ; float: right; "></td>
                 </tr>
             </table>
         </td>
     </tr>
     <tr>
         <td valign="top" align="left" bgcolor="#ffffff" style="color:#272c2b;font-family: 'Roboto', sans-serif;">
-            <table cellpadding="0" cellspacing="0" border="0" style="border-collapse: collapse; background-size: 100%;   margin: 0; " width="100%" class="t-content">
+            <table cellpadding="0" cellspacing="0" border="0"
+                   style="border-collapse: collapse; background-size: 100%;   margin: 0; " width="100%"
+                   class="t-content">
                 <tr>
                     <td style="padding: 30px 40px; background: #f9f9f9; " valign="top" class="">
-                        <h1  style="display: inline-block;font-family: 'Lato', sans-serif; margin-bottom: 20px; font-weight: 700; font-size: 16px; color: #272c2b;  text-transform: uppercase;">HELLO {{( $sendTo == 'carer') ? $carer->like_name : $purchaser->like_name}}</h1>
+                        <h1 style="display: inline-block;font-family: 'Lato', sans-serif; margin-bottom: 20px; font-weight: 700; font-size: 16px; color: #272c2b;  text-transform: uppercase;">
+                            HELLO {{( $sendTo == 'carer') ? $carer->like_name : $purchaser->like_name}}</h1>
                         <p style=" text-align: justify; font-weight: 300; margin: 10px 0;">
                             @if($sendTo == 'carer')
                                 <a href="{{route('ServiceUserProfilePublic',['serviceUserProfile'=>$serviceUser->id])}}">{{$serviceUser->first_name}}</a>
-                                offered an alternative time. Please click the button below to accept or reject the alternative time request.
+                                offered an alternative time. Please click the button below to accept or reject the
+                                alternative time request.
                             @else
-                                <a href="{{route('carerPublicProfile',['carerPublicProfile'=>$carer->id])}}">{{$carer->first_name}}</a> offered an alternative time.
+                                <a href="{{route('carerPublicProfile',['carerPublicProfile'=>$carer->id])}}">{{$carer->first_name}}</a>
+                                offered an alternative time.
                                 Please click the button below to accept or reject the alternative time request.
                             @endif
                         </p>
@@ -46,21 +76,22 @@
     </tr>
     <tr>
         <td style="color:#272c2b;padding:40px ;">
-            <table cellpadding="0" cellspacing="0" border="0" style="overflow-x: auto;border: 1px solid rgb(225, 225, 225);box-shadow: 0px 0px 26.73px 0.27px rgba(0, 0, 0, 0.11);border-collapse: collapse;   margin: 0; " width="100%" class="t-content">
+            <table cellpadding="0" cellspacing="0" border="0"
+                   style="overflow-x: auto;border: 1px solid rgb(225, 225, 225);box-shadow: 0px 0px 26.73px 0.27px rgba(0, 0, 0, 0.11);border-collapse: collapse;   margin: 0; "
+                   width="100%" class="t-content">
                 <tr>
                     <?php
-                    if($sendTo == 'carer'){
-                        $patchToSrvUserAvatar = getcwd().'/img/service_user_profile_photos/'.$serviceUser->id.'.png';
-                        if(file_exists($patchToSrvUserAvatar)) {
-                            $img_src = asset('/img/service_user_profile_photos/'.$serviceUser->id.'.png');
+                    if ($sendTo == 'carer') {
+                        $patchToSrvUserAvatar = getcwd() . '/img/service_user_profile_photos/' . $serviceUser->id . '.png';
+                        if (file_exists($patchToSrvUserAvatar)) {
+                            $img_src = asset('/img/service_user_profile_photos/' . $serviceUser->id . '.png');
                         } else {
                             $img_src = asset('/img/no_photo.png');
                         }
-                    }
-                    else{
-                        $patchToSrvUserAvatar = getcwd().'/img/profile_photos/'.$carer->id.'.png';
-                        if(file_exists($patchToSrvUserAvatar)) {
-                            $img_src = asset('/img/profile_photos/'.$carer->id.'.png');
+                    } else {
+                        $patchToSrvUserAvatar = getcwd() . '/img/profile_photos/' . $carer->id . '.png';
+                        if (file_exists($patchToSrvUserAvatar)) {
+                            $img_src = asset('/img/profile_photos/' . $carer->id . '.png');
                         } else {
                             $img_src = asset('/img/no_photo.png');
                         }
@@ -69,7 +100,8 @@
                     ?>
 
                     <td style="padding: 20px 15px 30px 15px;  " valign="middle">
-                        <div class="column" style="width:100%;max-width:200px;display:inline-block;vertical-align:middle;">
+                        <div class="column"
+                             style="width:100%;max-width:200px;display:inline-block;vertical-align:middle;">
                             <table width="100%" cellpadding="0" cellspacing="0" style="border-spacing:0;">
                                 <tr>
                                     <td align="center" style="text-align:left; ">
@@ -85,26 +117,36 @@
                             </table>
                         </div>
 
-                        <div class="column" style="width:100%;max-width:200px;display:inline-block;vertical-align:middle;;">
+                        <div class="column"
+                             style="width:100%;max-width:200px;display:inline-block;vertical-align:middle;;">
                             <table width="100%" cellpadding="0" cellspacing="0" style="border-spacing:0;">
                                 <tr>
                                     <td align="center" style="text-align:left;">
                                         @if($sendTo == 'carer')
                                             <p style=" margin-top: 10px;font-family: 'Lato', sans-serif;font-weight: 900; text-transform: uppercase;">
-                                                <a href="{{route('ServiceUserProfilePublic',['serviceUserProfile'=>$serviceUser->id])}}?referUserProfilePublic={{route('ServiceUserProfilePublic',['serviceUserProfile'=>$serviceUser->id])}}" style=" color: #6178fc;">
-                                                    {{$serviceUser->first_name}}</a><span style="display: block;">Booked you</span></p>
+                                                <a href="{{route('ServiceUserProfilePublic',['serviceUserProfile'=>$serviceUser->id])}}?referUserProfilePublic={{route('ServiceUserProfilePublic',['serviceUserProfile'=>$serviceUser->id])}}"
+                                                   style=" color: #6178fc;">
+                                                    {{$serviceUser->first_name}}</a><span style="display: block;">Booked you</span>
+                                            </p>
                                         @else
                                             <p style=" margin-top: 10px;font-family: 'Lato', sans-serif;font-weight: 900; text-transform: uppercase;">
-                                                <span style="display: block;">You booked</span><a href="{{route('carerPublicProfile',['carerPublicProfile'=>$carer->id])}}" style="color: #6178fc; font-weight: 900;">{{$carer->first_name}}</a></p>
+                                                <span style="display: block;">You booked</span><a
+                                                        href="{{route('carerPublicProfile',['carerPublicProfile'=>$carer->id])}}"
+                                                        style="color: #6178fc; font-weight: 900;">{{$carer->first_name}}</a>
+                                            </p>
                                     @endif
                                 </tr>
                             </table>
                         </div>
-                        <div class="column" style="width:100%;max-width:200px;display:inline-block;vertical-align:middle;;">
+                        <div class="column"
+                             style="width:100%;max-width:200px;display:inline-block;vertical-align:middle;;">
                             <table width="100%" cellpadding="0" cellspacing="0" style="border-spacing:0;">
                                 <tr>
                                     <td align="center" style="text-align:left;">
-                                        <p style="margin-top: 10px;  font-size: 16px;font-family: 'Roboto', sans-serif;"><span style="color: #909090;">{{$booking->appointments()->get()->count()}} Appointment{{$booking->appointments()->get()->count() > 1 ? 's':''}}</span></p>
+                                        <p style="margin-top: 10px;  font-size: 16px;font-family: 'Roboto', sans-serif;">
+                                            <span style="color: #909090;">{{$booking->appointments()->get()->count()}}
+                                                Appointment{{$booking->appointments()->get()->count() > 1 ? 's':''}}</span>
+                                        </p>
                                 </tr>
                             </table>
                         </div>
@@ -131,13 +173,14 @@
 
 
     </tr>
-    @if($sendTo == 'carer')
-        <tr>
-            <td valign="top" align="left" bgcolor="#ffffff">
-                <table cellpadding="0" cellspacing="0" border="0"
-                       style="border-collapse: collapse; background-size: 100%;   margin: 0; " width="100%"
-                       class="t-content">
-                    <tr>
+
+    <tr>
+        <td valign="top" align="left" bgcolor="#ffffff">
+            <table cellpadding="0" cellspacing="0" border="0"
+                   style="border-collapse: collapse; background-size: 100%;   margin: 0; " width="100%"
+                   class="t-content">
+                <tr>
+                    @if($sendTo == 'carer')
                         <td style="color:#272c2b;padding: 30px 10px; background: #f9f9f9; " valign="top" class="">
                             <a href="{{route('carerBooking',['status'=>'new'])}}?refer=booking" class=""
                                style="
@@ -159,11 +202,34 @@
                                 Click here to Accept or <br/>Reject Request
                             </a>
                         </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    @endif
+                    @else
+                        <td style="color:#272c2b;padding: 30px 10px; background: #f9f9f9; " valign="top" class="">
+                            <a href="{{route('purchaserBookingStatus',['status'=>'new'])}}?refer=booking" class=""
+                               style="
+                    display: block;
+                    width: 240px;
+                    line-height: 1.4;
+                    margin: 0 auto;
+                    padding: 10px 10px;
+                    border-radius: 30px;
+                    text-align: center;
+                    background: #80cb2d;
+                    text-transform: uppercase;
+                    color: #7bb7dc;
+                    font-family: 'Lato', sans-serif;
+                    font-weight: 900;
+                    color: #fff;
+                    font-size: 14px;
+                    text-decoration: none !important;">
+                                Click here to Accept or <br/>Reject Request
+                            </a>
+                        </td>
+                    @endif
+                </tr>
+            </table>
+        </td>
+    </tr>
+
     <tr>
         <td valign="top" align="left" bgcolor="#ffffff" style="font-family: 'Roboto', sans-serif;">
             <table cellpadding="0" cellspacing="0" border="0" style="    margin: 0; " width="100%" class="t-content">
@@ -174,7 +240,7 @@
                    text-transform: uppercase;
                    font-size: 14px;
                  margin-bottom:0;">
-                            Best wishes <br />
+                            Best wishes <br/>
                             The Holm Team
                         </p>
                         <a href="{{route('mainHomePage')}}" class=""
@@ -198,7 +264,9 @@
                 <tr>
                     <td style="padding:  0 40px 30px 40px; background: #fff; " valign="top" class="">
                         <p style="max-width: 600px;font-size: 15px; color: #373f3e;line-height: 1.6;">
-                            If you do not wish to receive promotional emails from Holm, <a href="{{route('unsubscribe',['id'=>Auth::user()->id])}}" class="">unsubscribe</a> here.
+                            If you do not wish to receive promotional emails from Holm, <a
+                                    href="{{route('unsubscribe',['id'=>Auth::user()->id])}}" class="">unsubscribe</a>
+                            here.
                         </p>
                         <p> You will continue to receive all other emails.</p>
                     </td>
