d05cda3426bde7d0a93acabe181c737d8003add1
AntonSh <anton.shelehvost@gmail.com>
2017-12-14 02:12:27 +0200

    HOLM-1124 PROFILES MANAGMENT

--- resources/views/HolmAdmin/profilesManagement/summaryTableRow.blade.php
+++ resources/views/HolmAdmin/profilesManagement/summaryTableRow.blade.php
@@ -1,8 +1,8 @@
 <tr>
     <td><span class="extraTitle">{{$item['type']}}</span></td>
-    <td>{{(isset($item['New']))?$item['New']:0}}</td>
-    <td>{{isset($item['Active'])?$item['Active']:0}}</td>
-    <td>{{isset($item['Rejected'])?$item['Rejected']:0}}</td>
-    <td>{{isset($item['Edited'])?$item['Edited']:0}}</td>
-    <td>{{isset($item['Blocked'])?$item['Blocked']:0}}</td>
+    <td style="text-align: center;">{{(isset($item['New']))?$item['New']:0}}</td>
+    <td style="text-align: center;">{{isset($item['Active'])?$item['Active']:0}}</td>
+    <td style="text-align: center;">{{isset($item['Rejected'])?$item['Rejected']:0}}</td>
+    <td style="text-align: center;">{{isset($item['Edited'])?$item['Edited']:0}}</td>
+    <td style="text-align: center;">{{isset($item['Blocked'])?$item['Blocked']:0}}</td>
 </tr>
