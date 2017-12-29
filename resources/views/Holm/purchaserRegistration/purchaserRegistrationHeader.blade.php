34096d34d1e4efa77f6702acd8234f366f2918aa
alexx <voloschinakate@gmail.com>
2017-12-29 18:42:52 +0200

    HOLM-1206 fix

--- resources/views/Holm/purchaserRegistration/purchaserRegistrationHeader.blade.php
+++ resources/views/Holm/purchaserRegistration/purchaserRegistrationHeader.blade.php
@@ -131,10 +131,12 @@
 
 </div>
 
-
+@if(env('APP_ENV') == 'production')
 <script type="text/javascript">
+
     $(window).on('load',function(){
         $('#PurchaserMessage').modal('show');
     });
 </script>
+@endif
 
