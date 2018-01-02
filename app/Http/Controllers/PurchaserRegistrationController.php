88669c3180c88a1c7fc70d186701c875a12c5909
alexx <voloschinakate@gmail.com>
2018-01-02 19:04:44 +0200

    HOLM-1214

--- app/Http/Controllers/PurchaserRegistrationController.php
+++ app/Http/Controllers/PurchaserRegistrationController.php
@@ -10,6 +10,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Mail;
 use Swift_TransportException;
 
