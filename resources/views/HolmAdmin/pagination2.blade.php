ca29233113f20c999d001701b921896f5c1ec0a5
alexx <voloschinakate@gmail.com>
2018-01-03 14:59:34 +0200

    HOLM-1189 Admin Panel

--- resources/views/HolmAdmin/pagination2.blade.php
+++ resources/views/HolmAdmin/pagination2.blade.php
@@ -9,13 +9,14 @@ if(isset($statusTypeFilter)) $extraGet.='&statusType='.$statusTypeFilter;
 @if($pages>1)
 <div class="paginationContainer">
     <div class="pagination">
-        <a href="{{($previousPage>=1)?$link.'/?page='.$previousPage.$extraGet:'#'}}" class="paginationArrow paginationArrow--left">
+        <a href="{{$link.'/?page='.$previousPage.$extraGet}}" class="paginationArrow paginationArrow--left" @if(($previousPage < 1))style="pointer-events: none;" @endif >
             <span><i class="fa fa-angle-left"></i></span>
         </a>
         @for($page=1; $page<=$pages; $page++)
             <a href="{{$link}}/?page={{$page.$extraGet}}" class="pagination__item @if($curr_page==$page) act @endif "><span>{{$page}}</span></a>
         @endfor
-        <a href="{{($nextPage<=$pages)?$link.'/?page='.$nextPage.$extraGet:'#'}}" class="paginationArrow paginationArrow--right">
+
+        <a href="{{$link.'/?page='.$nextPage.$extraGet}}" class="paginationArrow paginationArrow--right" @if(($nextPage > $pages))style="pointer-events: none;" @endif >
             <span><i class="fa fa-angle-right"></i></span>
         </a>
     </div>
